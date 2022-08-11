8976ebf222581ec14b6ca084cbd4c69329668270
samuel <samuel@sysfac.com.br>
2022-08-11 10:04:20 -0300

    layout finalizado

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import Filter from './components/filter';
 import Header from './components/header';
+import PieChartCard from './components/pie-chart-data';
 import SalesByDate from './components/sale-by-date';
 import SalesSummary from './components/sales-summary';
+import SalesTable from './components/sales-table';
 
 function App() {
   return (
@@ -15,7 +17,18 @@ function App() {
           <SalesByDate />
           <div className="sales-overview-container">
             <SalesSummary />
+            <PieChartCard
+              name="Lojas"
+              labels={['UberLaândia', 'Araguari', 'Uberaba']}
+              series={[40, 30, 30]}
+            />
+            <PieChartCard
+              name="Pagamentos"
+              labels={['Crédito', 'Débito', 'Dinheiro']}
+              series={[40, 30, 30]}
+            />
           </div>
+          <SalesTable />
         </div>
       </>
     </div>
